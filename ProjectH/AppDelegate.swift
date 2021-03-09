03802cdb8e46548217f02d3b51d4002ecab7f938
Ilyas Shomat <ilyas.shomat@gmail.com>
2021-03-09 17:32:04 +0600

    Added other modules

--- ProjectH/AppDelegate.swift
+++ ProjectH/AppDelegate.swift
@@ -6,31 +6,38 @@
 //
 
 import UIKit
+import ProjectAModule
+import ProjectBModule
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        
+        //MARK: - Setup Hybrid App
+//        setupHybridModule()
+        
+        //MARK: - Setup Project A
+        ProjectAModuleConnector.setupModule(appDelegate: self)
+
+        //MARK: - Setup Project B
+//        ProjectBModuleConnector.setupModule(appDelegate: self)
+        
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    
+    private func setupHybridModule() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(identifier: "test.test.ProjectH"))
+        let rootViewController = storyboard.instantiateViewController(withIdentifier: "ViewController")
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
